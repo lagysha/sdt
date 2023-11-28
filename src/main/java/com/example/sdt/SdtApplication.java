60c5806978f2a5076b18f80f2ff45cda3aef2f1a
Nikita Malov <nikita.volam@gmail.com>
2023-11-28 11:15:47 +0200

    initial commit

---
+++ src/main/java/com/example/sdt/SdtApplication.java
@@ -0,0 +1,13 @@
+package com.example.sdt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sdt/SdtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sdt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
